3fb1d613300e313b6b9ed068096ab07ba28647ea
lfrank01 <lfrank01@charlottesvilleschools.org>
2021-03-15 10:33:15 -0400

    Improved code comments and changed ready_to_feed to be False initially.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import adafruit_hcsr04
 # adafruit_hcsr04.mpy library can be found on the project repository:
 # https://github.com/vcraghe32/Pet_Food_Dispenser/blob/main/adafruit_hcsr04.mpy
 
-# The .mpy-cross program, which is linux-based, was used to convert -.py to .mpy.
+# The .mpy-cross program, which is linux-based, was used to convert -.py to -.mpy.
 
 # The link to adafruit_hcsr04.py source code:
 # https://github.com/adafruit/Adafruit_CircuitPython_HCSR04/blob/master/adafruit_hcsr04.py
@@ -38,6 +38,10 @@ CLOSED_ANGLE = 0.0
 
 # Also, functions are reusable in other code files. They can be copied and pasted or called using
 # "from (file) import (function)".
+
+# The functions could also be placed in an external file and imported like a library. They are put here for
+# convenience. If put in a library, such as LukeProject.py, they would be imported in this file like:
+# from LukeProject import initialize_lcd, feed_cycle
 
 
 def initialize_lcd(board, rows=2, columns=16):
@@ -73,15 +77,15 @@ def initialize_lcd(board, rows=2, columns=16):
 
     # OK, now talk to the LCD at the address that was just found by scanning.
 
-    lcd = LCD(I2CPCF8574Interface(device_address), num_rows=rows, num_cols=columns)
-
-    return lcd
+    return LCD(I2CPCF8574Interface(device_address), num_rows=rows, num_cols=columns)
+    # The return statement of a function is what it answers or hands back to the code that called it. In this case,
+    # it returns the initialized lcd object. The calling code can assign that to a variable and use it later to
+    # write to the LCD.
 
 
 def feed_cycle(duration, open_angle, closed_angle, lcd):
     # This function opens and closes the servo while displaying LCD messages.
 
-    # time_to_return = time.monotonic() + duration
     print("Activating feed cycle")
     lcd.clear()
     lcd.print("Servo\nForward")
@@ -94,41 +98,54 @@ def feed_cycle(duration, open_angle, closed_angle, lcd):
     my_servo.angle = closed_angle
     time.sleep(2)
 
-    return time.monotonic()  # As a return value, the time of the last feed cycle is handed back.
+    return (
+        time.monotonic()
+    )  # As a return value, the time of the last feed cycle is handed back.
 
 
 # Okay, the functions have been defined. Now, similar to how to an object is created from a library, objects will be
 # created from the functions.
 
-lcd = initialize_lcd(board)  # This is how to call the function that initializes the lcd. Note that the other arguments,
+lcd = initialize_lcd(
+    board
+)  # This is how to call the function that initializes the lcd. Note that the other arguments,
 # rows and columns, do not need to be specified because they are set to a default in the function. However, the defaults
 # could be changed by listing one of those arguments and setting it equal to a new number.
 
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D6)
 
 my_servo = servo.Servo(pulseio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50))
-# This line of code is basically using an external function. It lists the library then class and then arguments of that
-# class. Th
+# This line of code is basically using an external function. It lists the library, then class, and then arguments of
+# that class.
 
 # Important - The Digital IO pins that work with PWM: 1, 2, 3, 4, 5, 7, 9, 11, 12, and 13.
-# Not 0, 6, 8, and 10.
+# Digital IO pins 0, 6, 8, and 10 do not work.
 
 # Set the servo angle to a starting position
 my_servo.angle = CLOSED_ANGLE
 # Note how CLOSED_ANGLE was used instead of a number because it makes the code easier to understand.
 
-# Side note on Pycharm shortcuts: refactor (for renaming a variable) is shift-F6
+# Side note on Pycharm shortcuts: refactor (for renaming a variable) is shift-F6.
 
 print("Starting Main Loop...")
 
-ready_to_feed = True
+ready_to_feed = False
 print_time = time.monotonic()
 last_feed_time = time.monotonic()
 
 while True:
     time_remaining = last_feed_time + TIME_BTW_FEEDS - time.monotonic()
+    # Could time.monotonic() be compounding to make the time remaining value inaccurate?
 
     ready_to_feed = time_remaining < 0.0
+    # The above line isn't a calculation, it is an abbreviated "if" statement. The expression on the right of the equals
+    # is evaluated and the result is a yes or no (True or False). This means that ready_to_feed is set of that
+    # result of True or False. The line could be stated more verbosely like:
+
+    # if time_remaining < 0.0:
+    #    ready_to_feed = True
+    # else:
+    #    ready_to_feed = False
 
     # The code below gets the distance from the ultrasound sensor. This could be made into a function.
     try:
@@ -137,20 +154,47 @@ while True:
         )  # This continuously reads the distance measured every time the loop executes.
 
     except RuntimeError:
-        distance = (
-                TRIGGER_DISTANCE + 1.0
-        )
+        distance = TRIGGER_DISTANCE + 1.0
 
         # This code is very useful, as, without it, the code enters a blocked state with a RuntimeError when the
         # ultrasonic distance reads something too close. By setting the distance to 1 more than the triggering distance
         # when there is a RuntimeError, the distance will be converted into a "safe" value that will not trigger an
         # error nor activate the code.
 
-        print("Error - object too close to the ultrasound sensor")
+        print("Error - object too close to the ultrasonic distance sensor")
+
+    if (
+        print_time - time.monotonic() <= 0.0
+    ):  # This prints an update of the timer status every few seconds.
+        lcd.clear()
+
+        if time_remaining > 0.0:
+            print(
+                "Time left: "
+                + str(round(time_remaining))
+                + ". Closest object: "
+                + str(distance)
+            )
+            lcd.print("Ready in: \n")
+            lcd.print(str(round(time_remaining)))
+
+        else:
+            print(
+                "Ready to feed when distance sensor is triggered. Closest object: "
+                + str(distance)
+            )
+            lcd.print("Ready\nto feed")
+
+        print_time = (
+            LCD_REFRESH_TIMER + time.monotonic()
+        )  # This is the next print time.
 
     if distance <= TRIGGER_DISTANCE and ready_to_feed:
         # Note that the boolean (ready_to_feed) is set to true because, sensibly enough, it means that the code is ready
         # to feed. If it was activated by a variable called "not read_to_feed," that would be confusing.
+
+        # The things on the left side of the equals signs are the variable names used in the function's arguments list.
+        # The things on the right side of the equals signs are the constants that the arguments are set equal to.
 
         last_feed_time = feed_cycle(
             duration=CYCLE_TIME,
@@ -158,20 +202,16 @@ while True:
             closed_angle=CLOSED_ANGLE,
             lcd=lcd,
         )
+        # The feed_cycle function could also have no return value and just be executed without assigning any values
+        # to last_feed_time. If this were the case, the code would look like:
 
-        # The things on the left side of the equals signs are the arguments mentioned in the function.
-        # The things on the right side of the equals signs are the constants that the arguments are set equal to.
-
-    if print_time - time.monotonic() <= 0.0:  # This prints an update of the timer status every few seconds.
-        lcd.clear()
-
-        if time_remaining > 0.0:
-            print("Time left: " + str(round(time_remaining)) + ". Closest object: " + str(distance))
-            lcd.print("Ready in: \n")
-            lcd.print(str(round(time_remaining)))
-
-        else:
-            print("Ready to feed when distance sensor is triggered. Closest object: " + str(distance))
-            lcd.print("Ready\nto feed")
-
-        print_time = LCD_REFRESH_TIMER + time.monotonic()  # This is the next print time.
+        # feed_cycle(
+        #     duration=CYCLE_TIME,
+        #     open_angle=OPEN_ANGLE,
+        #     closed_angle=CLOSED_ANGLE,
+        #     lcd=lcd,
+        # )
+        # last_feed_time = time.monotonic()
+        # Since we always want to know when the last feed was completed, it is more efficient to put a return value in
+        # feed_cycle that gets time.monotonic() and hands it back to the calling code, where it can be assigned to a
+        # variable - in this case, last_feed_time.
